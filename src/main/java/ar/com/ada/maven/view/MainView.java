ebff4508277887d5769c6e7ce51e3aa76a248ce7
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2020-01-06 22:02:10 -0300

    Main view

--- src/main/java/ar/com/ada/maven/view/MainView.java
+++ src/main/java/ar/com/ada/maven/view/MainView.java
@@ -1,4 +1,14 @@
 package ar.com.ada.maven.view;
 
+import ar.com.ada.maven.utils.ScannerSingleton;
+
+import java.util.Scanner;
+
 public class MainView {
+
+    public Integer selectOption() {
+        System.out.println("APLICACIÓN DE ZOOLOGICO");
+        System.out.println("¿Con qué tabla desea operar? \nSi desea operar con 'CONTINENTE' ingrese[1], si desea salir ingrese [0]");
+        Scanner keyboard = ScannerSingleton.getScanner();
+    }
 }
